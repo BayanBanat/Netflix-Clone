0a435c9ae8238f0432d288bf79b871473d651274
BayanBanat <bayankbanat99@gmail.com>
2023-04-08 09:55:00 +0000

    class-18

--- netflex/src/components/Home.js
+++ netflex/src/components/Home.js
@@ -1,31 +1,23 @@
 import MovieList from './MovieList';
-import{ useState, useEffect } from 'react';
-function Home(){
-    const [movies,setMovies]=useState([])
+import { useState, useEffect } from 'react';
+function Home() {
+    const [movies, setMovies] = useState([])
 
-    async function getMovies(){
-        const url=`https://movies-library-fmzm.onrender.com`;
-        console.log(11111,url);
-
+    async function getMovies() {
+        const url = `https://movies-library-fmzm.onrender.com`;
         const response = await fetch(`${url}/trending`);
-        console.log(2222,response);
-
         const MoviesData = await response.json();
-        console.log(3333,MoviesData);
-       
         setMovies(MoviesData);
-        console.log(44444,movies)
-
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getMovies();
-    },[]);
+    }, []);
 
 
-    return(
+    return (
         <>
-         <MovieList movies={movies}/>
+            <MovieList movies={movies} />
         </>
     )
 }
